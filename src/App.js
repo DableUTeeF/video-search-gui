457c17dc922c65439e749790311c464c21066ed9
DableUTeeF <palm22180@gmail.com>
2025-09-08 11:52:05 +0700

    Added frameplayer

--- src/App.js
+++ src/App.js
@@ -1,23 +1,18 @@
-import logo from './logo.svg';
 import './App.css';
+import ReactPlayer from 'react-player'
+import React, { useState } from "react";
+import {FramePlayer} from './frameplayer'
 
 function App() {
+  const [videoFilePath, setVideoFilePath] = useState(null);
+  const handleVideoUpload = (event) => {
+    setVideoFilePath(URL.createObjectURL(event.target.files[0]));
+  };
+  console.log(videoFilePath);
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+      <input type="file" onChange={handleVideoUpload}/>
+      <video src={videoFilePath} width="100%" height="100%" controls={true} />
     </div>
   );
 }
